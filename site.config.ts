16a16508c451ed1564b6998568b6aff30010bb7a
Yoo Youngmo <gigamadness@gmail.com>
2022-05-11 16:41:45 +0900

    공용 workspace 로 변경

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '2af4277061cf4b4198c51c76bdaa4d50',
+  rootNotionPageId: '034971178b9c4bcd9611b3880998ad37',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
